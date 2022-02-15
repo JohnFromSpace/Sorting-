1793c4ca19a7219215c6d5f92f79ab82c7f660c3
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-16 01:23:21 +0200

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -16,5 +16,28 @@ void display(int* array, int size)
   cout << endl;
 }
 
+void bubbleSort(int* array, int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    int swaps = 0; //flag to detect if any swap is there or not
+    for(int j = 0; j < size - i - 1; j++)
+    {
+      if(array[j] > array[j + 1])//when the current element is bigger than the next one
+      {
+        swapping(array[j], array[j + 1]);
+        swaps = 1; //set swap flag
+      }
+    }
+    if(!swaps)
+      break;//No swap in this pass, so array is sorted
+  }
+}
+
+
+
+
+
+
 
 
