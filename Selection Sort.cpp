2cfc8cab850eb18182db31c8717d1c3b5bc5f4e6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-16 01:27:04 +0200

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -34,10 +34,27 @@ void bubbleSort(int* array, int size)
   }
 }
 
-
-
-
-
-
-
-
+int main()
+{
+  int n;
+  cout << "Enter the number of elements: ";
+  cin >> n;
+  
+  int arr[n];//create an array with given number of elements
+  cout << "Enter elements:" << endl;
+  
+  for(int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  
+  cout << "Array before sorting: ";
+  display(arr, n);
+  
+  bubbleSort(arr, n);
+  
+  cout << "Array after sorting: ";
+  display(arr, n);
+  
+  return 0;
+}
