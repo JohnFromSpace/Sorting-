cc54c3a3af0055861bdfd706fbe9525bb6d5d3ca
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-16 01:18:22 +0200

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -8,3 +8,13 @@ void swapping(int &a, int &b)
   a = b;
   b = temp;
 }
+
+void display(int* array, int size)
+{
+  for(int i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
+
+
+
