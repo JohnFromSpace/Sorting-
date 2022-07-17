c0a545491730c23d7977c1253b208dae10700b00
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-07-18 01:09:49 +0300

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -6,3 +6,20 @@ void print(int arr[], int n){
   }
   std::cout << std::endl;
 }
+
+void insertionSort(int arr[], int n){
+  for(int i = 1; i < n; i++){
+    int tempElement = arr[i];
+    j = i - 1;
+    
+    // Move elements of arr[0..i-1],  
+    // that are greater than tempElement, to one 
+    // position ahead of their 
+    // current position
+    while(j >= 0 && arr[j] > tempElement){
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = tempElement;    
+  }
+}
