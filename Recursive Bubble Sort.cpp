ed86316ec8725af9fb24f730b9a40e5bd3a55841
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-07-18 01:18:27 +0300

    Update Recursive Bubble Sort.cpp

--- Recursive Bubble Sort.cpp
+++ Recursive Bubble Sort.cpp
@@ -6,3 +6,27 @@ void print(int arr[], int n){
   }
   std::cout << std::endl;
 }
+
+void bubbleSort(int arr[], int n){
+  if(n == 1){
+    return;
+  }
+  
+  int counter = 0;
+  
+  //The largest number is moved to the end of the array.
+  for(int i = 0; i < n - 1; i++){
+    if(arr[i] > arr[i + 1]){
+      swap(arr[i], arr[i + 1]);
+      counter++;
+    }
+  }
+  
+  //Check if any recursion is happening
+  if(counter == 0){
+    return;
+  }
+  
+  //Largest element is fixed
+  bubbleSort(arr, n - 1);
+}
