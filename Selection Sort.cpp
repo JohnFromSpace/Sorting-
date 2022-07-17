8cd47c4fc0047e12a88930149a841e9592e8f0d3
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-07-18 00:54:09 +0300

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -12,3 +12,16 @@ void print(int arr[], int n){
   }
   std::cout << std::endl;
 }
+
+void selectionSort(int arr[], int n){
+  int minIndex;
+  for(int i = 0; i < n; i++){
+    minIndex = i;
+    for(int j = i + 1; j < n; j++){
+      if(arr[j] < arr[minIndex]){
+        minIndex = j;
+      }
+    }
+    swap(&arr[minIndex], &arr[i]);
+  }
+}
