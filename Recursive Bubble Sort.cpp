b4f5450f99198d6389cfd249cdca0b41a10eb22d
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-07-18 01:20:17 +0300

    Update Recursive Bubble Sort.cpp

--- Recursive Bubble Sort.cpp
+++ Recursive Bubble Sort.cpp
@@ -36,3 +36,14 @@ void bubbleSort(int arr[], int n){
   //Largest element is fixed
   bubbleSort(arr, n - 1);
 }
+
+int main(){
+  int arr[] = {64, 34, 25, 12, 22, 11, 90};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  
+  bubbleSort(arr, n);
+  printf("Sorted array : \n");
+  printArray(arr, n);
+  
+  return 0;
+}
