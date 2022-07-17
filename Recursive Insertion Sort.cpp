06ac214144f253aa9fcccfd0fcac0885d14735a8
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-07-18 01:37:35 +0300

    Update Recursive Insertion Sort.cpp

--- Recursive Insertion Sort.cpp
+++ Recursive Insertion Sort.cpp
@@ -6,3 +6,20 @@ void print(int arr[], int n){
   }
   std::cout << std::endl;
 }
+
+void recursiveInsertionSort(int arr[], int n){
+  if(n <= 1){
+    return;
+  }
+  
+  recursiveInsertionSort(arr, n - 1);
+  
+  int last = arr[n - 1];
+  int j = n - 2;
+  
+  while(j >= 0 && arr[j] > last){
+    arr[j + 1] = arr[j];
+    j = j - 1;
+  }
+  arr[j + 1] = last;
+}
